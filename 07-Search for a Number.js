d2c036a6b116a08aab2fa5e110da3ce9b4507256
Vasil Varshilov <40742216+VasilVarshilov@users.noreply.github.com>
2019-10-21 08:42:31 +0300

    Update 07-Search for a Number.js

--- 07-Search for a Number.js
+++ 07-Search for a Number.js
@@ -12,7 +12,7 @@ function solve(arr, items) {
             counter++;
         }
     }
-    console.log(`"Number ${numberToFind} occurs ${counter} times."`);
+    console.log(`Number ${numberToFind} occurs ${counter} times.`);
 }
 solve([5, 2, 3, 4, 1, 6],
     [5, 2, 3]);
